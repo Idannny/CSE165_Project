c1bf8023b8739b5d69c79564888364de99e8f173
LaFrance Daniels III <55370968+LDaniels18@users.noreply.github.com>
2021-05-06 03:11:40 -0700

    Added Button.h and Button.cpp

---
+++ ProjectCSE165/Button.cpp
@@ -0,0 +1,35 @@
+#include "Button.h" //including our button header file
+//#include "Game.h"
+
+
+
+Button::Button(QPushButton *parent): QPushButton(parent){
+    //creating the button:
+
+    my_button = new QPushButton("Reset", this); //"this" is the parent of the button now
+
+    //setting the size and location of the button:
+
+    my_button->setGeometry(QRect(QPoint(0,0) , QSize(100, 100))); //a rectangle at point (0,0) and size (100x100)
+
+
+    //connecting the button signal to the appropiate slot:
+
+    connect(my_button, &QPushButton::released, this, &Button::handleButton);
+
+
+
+} //end of main button definition
+
+//creating a function for the event of the button:
+
+void Button::handleButton(){
+
+    //change the text
+    my_button->setText("Something happens");
+
+    //resize the Button:
+    my_button->resize(100, 100);
+
+    //my_button->show();
+}
