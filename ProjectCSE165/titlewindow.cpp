5c2d5c17b00ea4e1f809e97bbeabf0378dd3ff88
LaFrance Daniels III <55370968+LDaniels18@users.noreply.github.com>
2021-05-10 15:40:15 -0700

    Added a new png file

--- ProjectCSE165/titlewindow.cpp
+++ ProjectCSE165/titlewindow.cpp
@@ -4,11 +4,28 @@
 
 extern Game * game;
 
+
 TitleWindow::TitleWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::TitleWindow)
 {
     ui->setupUi(this);
+
+
+
+    // Adding a song at the title screen:
+
+
+      QMediaPlayer * music = new QMediaPlayer();
+      music->setMedia(QUrl("qrc:/audio/Peaceful_Inrtro.wav")); //https://ia802607.us.archive.org/23/items/soundkeeperrecordings-formats/sr002-01-16
+      music->setVolume(50);
+      music->play();
+
+      show();
+
+
+
+
 }
 
 TitleWindow::~TitleWindow()
@@ -18,6 +35,8 @@ TitleWindow::~TitleWindow()
 
 void TitleWindow::on_pushButton_clicked()
 {
+
+
 
     //Effectively links two windows together:
 
